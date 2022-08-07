5d3a5ab1de42e68328f5554890038006055b5a72
Denishilo <denisanishenko@gmail.com>
2022-08-07 19:08:15 +0300

    feat: complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,21 @@
-class GuessingGame {
+class GuessingGame { 
     constructor() {}
-
+    start
+    end 
+    centre
     setRange(min, max) {
-
+        this.start=min
+        this.end=max
     }
-
     guess() {
-
+        this.centre= Math.ceil((this.end+this.start)/2)
+        return this.centre
     }
-
     lower() {
-
+        this.end = this.centre;
     }
-
     greater() {
-
+        this.start = this.centre
     }
 }
 
